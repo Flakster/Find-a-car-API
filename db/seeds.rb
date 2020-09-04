6518c45387d9ae876a4dfccc58bf4a26b7b0f7f5
flakster <carlos.santamaria@gmail.com>
2020-09-04 15:08:55 -0500

    Fix error in db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Car.create( { make: 'Renault', color: 'white', year: 2019, price: 12021 )
-Car.create( { make: 'Chevy', color: 'red', year: 2017, price: 1010 )
-Car.create( { make: 'Tesla', color: 'blue', year: 2015, price: 12098 )
-Car.create( { make: 'Audi', color: 'silver', year: 2019, price: 13093 )
-Car.create( { make: 'Nissan', color: 'green', year: 2018, price: 15000 )
+Car.create( { make: 'Renault', color: 'white', year: 2019, price: 12021 })
+Car.create( { make: 'Chevy', color: 'red', year: 2017, price: 1010 } )
+Car.create( { make: 'Tesla', color: 'blue', year: 2015, price: 12098 } )
+Car.create( { make: 'Audi', color: 'silver', year: 2019, price: 13093 } )
+Car.create( { make: 'Nissan', color: 'green', year: 2018, price: 15000 } )
